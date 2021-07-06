ab922ac785226861ba3fea716be56da5a0af155b
Herman Chan <hermanchan@Hermans-MacBook-Pro.local>
2021-07-06 01:04:33 -0400

    add return value to logEvent

--- ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
+++ ios/Classes/SwiftFlutterFacebookSdkPlugin.swift
@@ -284,6 +284,8 @@ public class SwiftFlutterFacebookSdkPlugin: NSObject, FlutterPlugin, FlutterStre
             }
             if let myArgs = args as? [String: Any]{
                 logGenericEvent(args: myArgs)
+                result(true)
+                return
             }
             
         default:
